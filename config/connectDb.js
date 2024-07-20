8f4b44dfbb2f24efa1a818a53588acc55cf730c9
Sridhar Baindla <sridharbaindla5121@gmail.com>
2024-07-21 00:04:52 +0530

    Updated connectDb.js file

--- config/connectDb.js
+++ config/connectDb.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 const connectDb = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/expenses"); // Replace "process.env.MONGO_URL" with your MongoDB connection String or assign connection string to MONGO_URL in .env file. Note: Connection string should be upto database name specified which is already created.
+    await mongoose.connect(process.env.MONGO_URL); // Replace "process.env.MONGO_URL" with your MongoDB connection String (or) assign connection string to MONGO_URL in .env file. Note: Connection string should be upto database name specified which is already created.
     console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
   } catch (error) {
     console.log(`${error}`.bgRed);
